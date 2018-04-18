ca62f6c8f425b74ed07195933584066417bb3e63
华丽萍 <hualiping@hualipingdeMacBook-Pro.local>
2018-04-18 20:23:16 +0800

    add
    add

--- iEat/app/src/main/java/Fragments/PersonFragment.java
+++ iEat/app/src/main/java/Fragments/PersonFragment.java
@@ -1,24 +1,48 @@
 package Fragments;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
+import com.example.ieat.LoginActivity;
 import com.example.ieat.R;
 
 public class PersonFragment extends Fragment {
+    private ImageButton person_head=null;
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onAttach(Activity activity) {
+        // TODO Auto-generated method stub
+        super.onAttach(activity);
     }
+
     @Override
     public View onCreateView(LayoutInflater inflater,ViewGroup container,  Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.fragment_person, null);
         return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+        addListenerOnButton();
+    }
+    private void addListenerOnButton() {
+        person_head=(ImageButton)getView().findViewById(R.id.person_head);
+        person_head.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
     @Override
     public void onPause(){
